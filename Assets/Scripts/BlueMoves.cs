fdd66a576388019cac0cb7b9c6adf5a6e6112f82
Seb Cossu <smcossu@gmail.com>
2018-07-26 16:53:23 +0100

    Adding Files
    Adding Files

---
+++ Assets/Scripts/BlueMoves.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlueMoves : MonoBehaviour
+{
+    [SerializeField] private float speed = 10.0F;
+    [SerializeField] private float rotationSpeed = 100.0F;
+
+    void Update()
+    {
+    	KbMoving();
+    }
+
+    private void KbMoving()
+    {
+        float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+    }
+}
---
+++ Assets/Scripts/RedFollows.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedFollows : MonoBehaviour
+{
+    [SerializeField] Transform goal;
+    [SerializeField] float accuracy = 1.0f;
+    [SerializeField] float speed = 8.0f;
+    [SerializeField] float rotSpeed = 100.0f;
+
+	void LateUpdate ()
+	{
+		Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+		Vector3 direction  = lookAtGoal - this.transform.position;
+
+		this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+
+		if(Vector3.Distance(transform.position, lookAtGoal) > accuracy)
+		{
+	        this.transform.Translate(0, 0, speed * Time.deltaTime);
+		}
+	}
+}
